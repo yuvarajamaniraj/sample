3c8ebc6254059a7bcfc16e80a717421385019f09
yuvarajamaniraj <50545100+yuvarajamaniraj@users.noreply.github.com>
2020-11-27 16:52:13 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const inputs = document.querySelectorAll(".form__input")
+    
+    function addfocus(){
+        let parent = this.parentNode.parentNode
+        parent.classList.add("focus")
+    }
+    function remfocus(){
+        let parent = this.parentNode.parentNode
+        if(this.value == ""){
+            parent.classList.remove("focus")
+        }
+    }
+    inputs.forEach(input=>{
+        input.addEventListener("focus",addfocus)
+        input.addEventListener("blur",remfocus)
+    })
